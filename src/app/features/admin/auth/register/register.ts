ffdb007ee0dba2f3e6b13dc1559201c70ffa19a7
joycetagbovi <joyceabena19@gmail.com>
2025-07-15 18:17:52 +0000

    create registration form

--- src/app/features/admin/auth/register/register.ts
+++ src/app/features/admin/auth/register/register.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormInput } from '../../../../shared/components/form-input/form-input';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true,
   templateUrl: './register.html',
+  imports: [FormInput, RouterLink],
   styleUrl: './register.css'
 })
 export class Register {
