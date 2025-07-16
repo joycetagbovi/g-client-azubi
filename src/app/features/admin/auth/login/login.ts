278e5ee1d638275e498435348465bce451b5d8a2
joycetagbovi <joyceabena19@gmail.com>
2025-07-16 11:27:54 +0000

    create login page ui

--- src/app/features/admin/auth/login/login.ts
+++ src/app/features/admin/auth/login/login.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormInput } from '../../../../shared/components/form-input/form-input';
+import { RouterLink } from '@angular/router';
 
 @Component({
+  standalone: true,
+  imports: [FormInput, RouterLink],
   selector: 'app-login',
   templateUrl: './login.html',
   styleUrl: './login.css'
