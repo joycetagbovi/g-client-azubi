2010387d4cd7065c626ba434fb08c68e8c9c5e24
joycetagbovi <joyceabena19@gmail.com>
2025-07-16 11:28:09 +0000

    create reset password page

--- src/app/features/admin/auth/reset-password/reset-password.ts
+++ src/app/features/admin/auth/reset-password/reset-password.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormInput } from '../../../../shared/components/form-input/form-input';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-reset-password',
-  imports: [],
+  standalone: true,
+  imports: [FormInput, RouterLink, FormsModule,CommonModule],
   templateUrl: './reset-password.html',
   styleUrl: './reset-password.css'
 })
 export class ResetPassword {
+  isResettingPassword: boolean = false;
+
+    onSubmit() {
+    if (!this.isResettingPassword) {
+      // Step 1: Handle email submission
+      console.log('Submitting email...');
+      this.isResettingPassword = true;
+    } else {
+      // Step 2: Handle password reset
+      console.log('Setting new password...');
+    }
+  }
 
 }
