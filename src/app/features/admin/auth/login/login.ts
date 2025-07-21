e8ec6d038688dc7af5504dac1e94bc32436b8c2a
joycetagbovi <joyceabena19@gmail.com>
2025-07-21 08:06:08 +0000

    login ui and api intergration

--- src/app/features/admin/auth/login/login.ts
+++ src/app/features/admin/auth/login/login.ts
@@ -1,15 +1,74 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { FormInput } from '../../../../shared/components/form-input/form-input';
-import { RouterLink } from '@angular/router';
+import { RouterLink,Router } from '@angular/router';
+import {ReactiveFormsModule }from '@angular/forms';
+import { AuthService } from '../../../../core/services/auth.service';
+import { ToastService } from '../../../../core/services/toast.service';
+import { CommonModule } from '@angular/common';
+
+export interface LoginErrorResponse {
+  success: boolean;
+  errors: Array<{
+    message: string;
+  }>;
+}
 
 @Component({
   standalone: true,
-  imports: [FormInput, RouterLink],
+  imports: [FormInput, RouterLink, ReactiveFormsModule, CommonModule],
   selector: 'app-login',
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+   loginForm: FormGroup;
+  isLoading = false;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+    private toastService: ToastService
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  get f() {
+    return this.loginForm.controls;
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.invalid) {
+      this.toastService.error('Please enter valid email and password.');
+      return;
+    }
+
+    this.isLoading = true;
+
+    const { email, password } = this.loginForm.value;
+
+    this.authService.login({email, password}).subscribe({
+      next: (response:any) => {
+        if (response.success) {
+          this.toastService.success('Login successful!');
+          this.router.navigate(['/admin/dashboard']);
+        } else {
+          const errorMessage = response.errors?.[0]?.message || 'Login failed. Please try again.';
+          this.toastService.error(errorMessage);
+        }
+        this.isLoading = false;
+      },
+      error: (errorResponse: { error: LoginErrorResponse })=> {
+        const errorMessage = errorResponse.error.errors?.[0]?.message ||
+        'Something went wrong. Please try again.';
+      this.toastService.error(errorMessage);
+      this.isLoading = false;
+      }
+    });
+  }
 
 }
