3639b6e25be12e7b8d8ed56464fb9852631bdd70
joycetagbovi <joyceabena19@gmail.com>
2025-07-28 13:42:20 +0000

    update admin layout

--- src/app/layouts/admin-layout/admin-layout.ts
+++ src/app/layouts/admin-layout/admin-layout.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { Sidebar } from '../../features/admin/components/sidebar/sidebar';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AdminHeaderComponent } from './admin-header.component';
 
 @Component({
   selector: 'app-admin-layout',
-  imports: [],
+  imports: [Sidebar, RouterOutlet, CommonModule, AdminHeaderComponent],
   templateUrl: './admin-layout.html',
   styleUrl: './admin-layout.css'
 })
