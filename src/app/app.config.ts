1d6eea576b41039086ac3ec91b190d7bddead7f5
joycetagbovi <joyceabena19@gmail.com>
2025-07-28 13:35:25 +0000

    adding authinterceptors and route guard

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,13 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi , HTTP_INTERCEPTORS} from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 import {MessageService} from 'primeng/api';
+import {AuthInterceptor} from './core/interceptors/auth.interceptors';
+import {AuthService} from './core/services/auth.service';
+import {AuthGuard} from './core/guards/auth.guard';
 
 
 import { routes } from './app.routes';
@@ -21,6 +24,8 @@ export const appConfig: ApplicationConfig = {
       preset: Aura,
       }
     }),
-    MessageService
+    AuthService,
+    MessageService,
+    {provide:  HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'admin', pathMatch: 'full' },
+     {
+    path: 'reset-password/:token',
+    loadComponent: () => import('./features/admin/auth/reset-password/reset-password').then(c => c.ResetPassword)
+  },
+  
     { path: 'learner', loadComponent: () => import('./features/learner/learner').then(m => m.Learner) },
     { 
         path: 'admin', 
