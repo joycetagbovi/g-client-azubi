d2019aab74006d01e9e0642147ec91dc51b062d7
joycetagbovi <joyceabena19@gmail.com>
2025-07-28 13:42:32 +0000

    update learners route

---
+++ src/app/features/learner/learner.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+
+export const learnerRoutes: Routes = [
+    {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+    },
+   
+]; 
