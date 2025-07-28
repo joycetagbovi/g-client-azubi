279bb6ed913a8f7464351ddef2f88e8d7060b280
joycetagbovi <joyceabena19@gmail.com>
2025-07-28 13:39:41 +0000

    adding top nav component

---
+++ src/app/features/admin/components/navbar/navbar.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [CommonModule],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css'
+})
+export class Navbar {
+  @Input() title: string = '';
+   @Input() paragraph: string = '';
+
+}
