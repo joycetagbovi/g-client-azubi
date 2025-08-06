8e75a8b92e6520698d1991474f772ef813a6a53a
joycetagbovi <joyceabena19@gmail.com>
2025-08-06 10:20:04 +0000

    update admin route

--- src/app/features/admin/admin.routes.ts
+++ src/app/features/admin/admin.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import {AuthGuard } from '../../core/guards/auth.guard';
+import { AuthGuard } from '../../core/guards/auth.guard';
 
 export const adminRoutes: Routes = [
   {
@@ -13,37 +13,41 @@ export const adminRoutes: Routes = [
     pathMatch: 'full'
   },
 
-{
-  path: '',
-  loadComponent: () => import('../../layouts/admin-layout/admin-layout').then(c => c.AdminLayout),
-  canActivate: [AuthGuard],
-  children: [
- {
-    path: 'dashboard',
-    loadComponent: () => import('./pages/dashboard/dashboard').then(m => m.Dashboard)
-  },
   {
-    path: 'courses',
-    loadComponent: () => import('./pages/courses/courses').then(m => m.Courses)
-  },
-  {
-    path: 'tracks',
-    loadComponent: () => import('./pages/tracks/tracks').then(m => m.Tracks)
-  },
-  {
-    path: 'invoices',
-    loadComponent: () => import('./pages/invoices/invoices').then(m => m.Invoices)
-  },
-  {
-    path: 'learners',
-    loadComponent: () => import('./pages/learners/learners').then(m => m.Learners)
-  },
-  {
-    path: 'reports',
-    loadComponent: () => import('./pages/reports/reports').then(m => m.Reports)
+    path: '',
+    loadComponent: () => import('../../layouts/admin-layout/admin-layout').then(c => c.AdminLayout),
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: 'dashboard',
+        loadComponent: () => import('./pages/dashboard/dashboard').then(m => m.Dashboard)
+      },
+      {
+        path: 'courses',
+        loadComponent: () => import('./pages/courses/courses').then(m => m.Courses)
+      },
+      {
+        path: 'tracks',
+        loadComponent: () => import('./pages/tracks/tracks').then(m => m.Tracks)
+      },
+      {
+        path: 'tracks/:id',
+        loadComponent: () => import('./pages/track-details/track-details').then(m => m.TrackDetails)
+      },
+      {
+        path: 'invoices',
+        loadComponent: () => import('./pages/invoices/invoices').then(m => m.Invoices)
+      },
+      {
+        path: 'learners',
+        loadComponent: () => import('./pages/learners/learners').then(m => m.Learners)
+      },
+      {
+        path: 'reports',
+        loadComponent: () => import('./pages/reports/reports').then(m => m.Reports)
+      }
+    ]
   }
-  ]
-}
- 
+
 ];
 
